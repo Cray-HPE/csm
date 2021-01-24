aec4bf71cc6eb4ab2cb4336cfedf5ae7c11ddbc2
Russell Bunch <rbunch@cray.com>
2021-01-24 15:44:10 -0600

    CASMREL-539 New LiveCD and NCN Images

--- release.sh
+++ release.sh
@@ -103,7 +103,7 @@ reposync "http://dst.us.cray.com/dstrepo/bloblets/shasta-firmware/${BLOBLET_REF}
 
 # Download pre-install toolkit
 # NOTE: This value is printed in #livecd-ci-alerts (slack) when a build STARTS.
-: "${CRAY_PIT_VERSION:="sle15sp2.x86_64-1.3.0-20210123000044-g6527f47"}"
+: "${CRAY_PIT_VERSION:="sle15sp2.x86_64-1.3.1-20210124232530-gf90b504"}"
 : "${CRAY_PIT_URL:="http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/${BLOBLET_REF}/metal-team/cray-pre-install-toolkit-${CRAY_PIT_VERSION}.iso"}"
 (
     cd "${BUILDDIR}"
@@ -123,7 +123,7 @@ curl -sfSL "${CRAY_PIT_URL%.iso}.packages" \
 
 # Download Kubernetes images
 : "${KUBERNETES_IMAGES_URL:="https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes"}"
-: "${KUBERNETES_IMAGE_VERSION:="0.0.16"}"
+: "${KUBERNETES_IMAGE_VERSION:="0.0.17"}"
 (
     mkdir -p "${BUILDDIR}/images/kubernetes"
     cd "${BUILDDIR}/images/kubernetes"
@@ -134,7 +134,7 @@ curl -sfSL "${CRAY_PIT_URL%.iso}.packages" \
 
 # Download Ceph images
 : "${STORAGE_CEPH_IMAGES_URL:="https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph"}"
-: "${STORAGE_CEPH_IMAGE_VERSION:="0.0.14"}"
+: "${STORAGE_CEPH_IMAGE_VERSION:="0.0.15"}"
 (
     mkdir -p "${BUILDDIR}/images/storage-ceph"
     cd "${BUILDDIR}/images/storage-ceph"
