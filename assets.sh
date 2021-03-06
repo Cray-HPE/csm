8cb981b1d12a7798a71d7638d24c27cfa12dca35
Jenkins CSM Release Job <jenkins@hpe.com>
2021-03-06 11:37:47 -0600

    Updating assets.sh for 0.8.21-alpha.2 CASMINST-1231

--- assets.sh
+++ assets.sh
@@ -3,21 +3,21 @@
 # Copyright 2021 Hewlett Packard Enterprise Development LP
 
 PIT_ASSETS=(
-    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/release/shasta-1.4/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.9-20210305203641-g1e67449.iso
-    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/release/shasta-1.4/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.9-20210305203641-g1e67449.packages
-    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/release/shasta-1.4/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.9-20210305203641-g1e67449.verified
+    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/release/shasta-1.4/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.9-20210306091533-g1e67449.iso
+    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/release/shasta-1.4/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.9-20210306091533-g1e67449.packages
+    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/release/shasta-1.4/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.9-20210306091533-g1e67449.verified
 )
 
 KUBERNETES_ASSETS=(
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.0.52-2/kubernetes-0.0.52-2.squashfs
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.0.52-2/5.3.18-24.49-default-0.0.52-2.kernel
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.0.52-2/initrd.img-0.0.52-2.xz
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.0.52-3/kubernetes-0.0.52-3.squashfs
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.0.52-3/5.3.18-24.49-default-0.0.52-3.kernel
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.0.52-3/initrd.img-0.0.52-3.xz
 )
 
 STORAGE_CEPH_ASSETS=(
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.0.43-2/storage-ceph-0.0.43-2.squashfs
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.0.43-2/5.3.18-24.49-default-0.0.43-2.kernel
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.0.43-2/initrd.img-0.0.43-2.xz
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.0.43-3/storage-ceph-0.0.43-3.squashfs
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.0.43-3/5.3.18-24.49-default-0.0.43-3.kernel
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.0.43-3/initrd.img-0.0.43-3.xz
 )
 
 set -exo pipefail
