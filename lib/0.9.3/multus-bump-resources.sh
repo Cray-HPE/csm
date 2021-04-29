6e1f01016770e5373fb1b36eda4303aff6517493
Kim Jensen <kim.jensen@hpe.com>
2021-04-29 15:07:58 -0600

    CASMPET-4028 : tweak multus script to use k8s output instead of disk config file on m002

--- lib/0.9.3/multus-bump-resources.sh
+++ lib/0.9.3/multus-bump-resources.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Applying new resource limits to /etc/cray/kubernetes/multus-daemonset.yml"
-mfile=/etc/cray/kubernetes/multus-daemonset.yml
+echo "Applying new resource limits to kube-multus pods"
+mfile=/tmp/multus-daemonset.yml
+kubectl -n kube-system get daemonset kube-multus-ds-amd64 -o yaml > $mfile
 yq w -i --doc 5 $mfile 'spec.template.spec.containers.(name==kube-multus).resources.limits.memory' '100Mi'
 kubectl apply -f $mfile
