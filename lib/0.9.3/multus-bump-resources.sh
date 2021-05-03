a2752d8ffcfc4404016cb427947c2820e1c81c75
Kim Jensen <kim.jensen@hpe.com>
2021-05-03 12:23:56 -0600

    CASMINST-2127 : coredns-bump-resources.sh and multus-bump-resources.sh not executable

--- lib/0.9.3/multus-bump-resources.sh
+++ lib/0.9.3/multus-bump-resources.sh
@@ -2,5 +2,5 @@
 echo "Applying new resource limits to kube-multus pods"
 mfile=/tmp/multus-daemonset.yml
 kubectl -n kube-system get daemonset kube-multus-ds-amd64 -o yaml > $mfile
-yq w -i --doc 5 $mfile 'spec.template.spec.containers.(name==kube-multus).resources.limits.memory' '100Mi'
+yq w -i $mfile 'spec.template.spec.containers.(name==kube-multus).resources.limits.memory' '100Mi'
 kubectl apply -f $mfile
