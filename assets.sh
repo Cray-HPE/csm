56f24985607326f9c7f83276d74cf53cc92ee772
John Heemstra <heemstra@hpe.com>
2021-05-05 15:16:17 -0400

    CASMINST-2064: update NCN image versions
    Update to the latest storage/k8s NCN images.  What's new
    compared to 0.1.24:
    
    * new node-image-non-compute common with an updated set of
      base packages for the NCNs.  Among these updates is a newer
      version of cray-network-config that has a fix for NETETH-1275
    
    There were no ceph or k8s related changes.  Those images were
    simply rebuilt to pick up the new common layer.
    
    Following is the full list of updates:
    
    -cfs-state-reporter=1.5.3-20210331154622_d0d0346
    +cfs-state-reporter=1.5.4-20210430131530_c6ad1d5
    
    -cray-cos-release=1.0.1-20210303201113_3b69a58
    -cray-dvs-kmp-default=2.12_4.0.100_k5.3.18_24.46-1.4.0.0_7.1__gca5ff5bc
    -cray-dvs-service=2.12_4.0.100-1.4.0.0_7.1__gca5ff5bc
    -cray-heartbeat=1.0.44-7.0.1.0_1.2__gdd11601.shasta
    +cray-cos-release=1.0.1-20210502211115_3b69a58
    +cray-dvs-kmp-default=2.12_4.0.104_k5.3.18_24.52-1.4.0.0_11.1__g8e92e740
    +cray-dvs-service=2.12_4.0.104-1.4.0.0_11.1__g8e92e740
    +cray-heartbeat=1.0.46-1.4.0.0_4.1__g14563d6.shasta
    
    -cray-lustre-client-kmp-default=2.12.4.2_cray_18_g0120bae_k5.3.18_24.46-1.4.0.0_2.14__g0120bae110.shasta
    +cray-lustre-client-kmp-default=2.12.4.2_cray_18_g0120bae_k5.3.18_24.52-1.4.0.0_2.18__g0120bae110.shasta
    
    -cray-node-identity=0.4.4-20201209110544_177bdcd
    -cray-power-button=1.2.1-20201209110558_b1c1c7e
    +cray-node-identity=0.4.4-20210407142127_177bdcd
    +cray-power-button=1.2.1-20210407142215_b1c1c7e
    
    -cloud-init=20.2-8.39.8
    -craycli=0.40.9-20210408072059_4dfdfcf
    +cloud-init=20.2-8.45.1
    +craycli=0.40.15-20210430145614_ace95bd
    
    -dracut-kiwi-live=9.21.23-3.30.2
    -hpe-csm-scripts=0.0.9-20210325201219_b15e8e1
    +dracut-kiwi-live=9.23.20-3.37.1
    +hpe-csm-scripts=0.0.9-20210426150421_291073d
    
    -mdadm=4.1-15.26.1
    +mdadm=4.1-15.29.1
    
    -csm-testing=1.6.6-20210423144707_096f25f
    -goss-servers=1.6.6-20210423144707_096f25f
    -hpe-csm-goss-package=0.3.13-20210127124704_aae8d77
    +csm-testing=1.6.6-20210430115243_7c613b7
    +goss-servers=1.6.6-20210430115243_7c613b7
    +hpe-csm-goss-package=0.3.13-20210127124706_aae8d77
    
    -cray-network-config=1.1.2-20210105144415_f586e0b
    -slingshot-network-config=1.1.5-20210104144531_17eb00c
    -slingshot-utils=1.0.1-20210107174624_cc96eab
    +cray-network-config=1.1.7-20210318094807_b409053
    +slingshot-network-config=1.1.7-20210322150135_b7a9e10
    +slingshot-utils=1.0.2-20210322150143_4679f59
    
    -crash=7.2.8-18.2.1
    +crash=7.2.8-18.4.8
    
    -gdb=10.1-8.21.2
    +gdb=10.1-8.24.1
    
    -git-core=2.26.2-3.28.2
    +git-core=2.26.2-3.31.1
    
    -libunwind-devel=1.2.1-4.2.3
    +libunwind-devel=1.5.0-4.5.1
    
    -pdsh=2.34-2.17
    +pdsh=2.34-4.3.9
    
    -python3-colorama=0.3.9-1.24
    +python3-colorama=0.4.4-5.4.1
    
    -rsyslog=8.39.0-4.7.3
    +rsyslog=8.39.0-4.10.1
    
    -spire-agent=0.12.0-1_20210301152933_2d06876
    +spire-agent=0.12.0-1_20210407135708_2d06876
    
    -tcpdump=4.9.2-3.12.1
    +tcpdump=4.9.2-3.15.1

--- assets.sh
+++ assets.sh
@@ -9,15 +9,15 @@ PIT_ASSETS=(
 )
 
 KUBERNETES_ASSETS=(
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.1.24/kubernetes-0.1.24.squashfs
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.1.24/5.3.18-24.52-default-0.1.24.kernel
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.1.24/initrd.img-0.1.24.xz
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.1.25/kubernetes-0.1.25.squashfs
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.1.25/5.3.18-24.52-default-0.1.25.kernel
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.1.25/initrd.img-0.1.25.xz
 )
 
 STORAGE_CEPH_ASSETS=(
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.1.24/storage-ceph-0.1.24.squashfs
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.1.24/5.3.18-24.52-default-0.1.24.kernel
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.1.24/initrd.img-0.1.24.xz
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.1.25/storage-ceph-0.1.25.squashfs
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.1.25/5.3.18-24.52-default-0.1.25.kernel
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.1.25/initrd.img-0.1.25.xz
 )
 
 FIRMWARE_PACKAGE=http://car.dev.cray.com/artifactory/internal/~PVIRTUCIO/release/Cray_Firmware/03.04.2021_v1/firmware_package_03042021a.tgz
