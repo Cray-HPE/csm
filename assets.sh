909769e36bec87988cc997a629ea957d4ee5a44b
John Heemstra <heemstra@hpe.com>
2021-05-10 14:26:07 -0400

    CASMINST-2162 CASMPET-4119: assets update for ceph, k8s, liveCD images
    LiveCD: cray-pre-install-toolkit-sle15sp2.x86_64-1.4.10-20210510065410-gc054094.iso
    	• Version:
    	• CSI changes: https://stash.us.cray.com/projects/CSM/repos/csm/pull-requests/561/overview
    
    ncn-common:
    	• Version: 0.1.30
    	• Changes:
    		○ CASMINST-2162
    
    ncn-storage-ceph:
    	• Version: 0.1.28
    	• Changes since 0.1.26-2:
    		○ Pre-create zone1.rgw.buckets.data so it can be tuned, also reorder ssh key scan
    		○ Add function to enable sts for 1.5 fresh install
    
    ncn-k8s:
    	• Version: 0.1.27
    	• Changes since 0.1.26-3/0.1.26-2/0.1.26-1: CASMPET-4119

--- assets.sh
+++ assets.sh
@@ -3,21 +3,21 @@
 # Copyright 2021 Hewlett Packard Enterprise Development LP
 
 PIT_ASSETS=(
-    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/dev/master/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.10-20210506065412-gc054094.iso
-    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/dev/master/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.10-20210506065412-gc054094.packages
-    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/dev/master/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.10-20210506065412-gc054094.verified
+    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/dev/master/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.10-20210510065410-gc054094.iso
+    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/dev/master/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.10-20210510065410-gc054094.packages
+    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/dev/master/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.10-20210510065410-gc054094.verified
 )
 
 KUBERNETES_ASSETS=(
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.1.26-3/kubernetes-0.1.26-3.squashfs
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.1.26-3/5.3.18-24.52-default-0.1.26-3.kernel
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.1.26-3/initrd.img-0.1.26-3.xz
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.1.27/kubernetes-0.1.27.squashfs
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.1.27/5.3.18-24.52-default-0.1.27.kernel
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/kubernetes/0.1.27/initrd.img-0.1.27.xz
 )
 
 STORAGE_CEPH_ASSETS=(
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.1.26-2/storage-ceph-0.1.26-2.squashfs
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.1.26-2/5.3.18-24.52-default-0.1.26-2.kernel
-    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.1.26-2/initrd.img-0.1.26-2.xz
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.1.28/storage-ceph-0.1.28.squashfs
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.1.28/5.3.18-24.52-default-0.1.28.kernel
+    https://arti.dev.cray.com/artifactory/node-images-unstable-local/shasta/storage-ceph/0.1.28/initrd.img-0.1.28.xz
 )
 
 FIRMWARE_PACKAGE=http://car.dev.cray.com/artifactory/internal/~PVIRTUCIO/release/Cray_Firmware/03.04.2021_v1/firmware_package_03042021a.tgz
