2c5fc543d8de318a58e620f0aac4ef631d9d2ebb
Zach Crisler <zachary.crisler@hpe.com>
2021-05-10 23:56:28 -0400

    Add unguiculus/docker-python3-phantomjs-selenium image

--- docker/transform.sh
+++ docker/transform.sh
@@ -37,6 +37,7 @@ DISTDIR=$1
     mv docker.io/prom/* prom/
     mv quay.io/prometheus/* prometheus/
     mv docker.io/weaveworks/ weaveworks/
+    mv docker.io/unguiculus/ unguiculus/
     mv gcr.io/spiffe-io/ spiffe-io/
     find . -name 'kube-*-amd64:*' | while read name; do mv "$name" "$(echo "$name" | sed -e 's/-amd64//')"; done
     mv quay.io/cephcsi/ cephcsi/
