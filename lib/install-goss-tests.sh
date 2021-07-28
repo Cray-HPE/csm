9dc8df1e8a3762563c0bc9fda90b7dcb3cb1dd70
Brad Klein <bklein@cray.com>
2021-07-28 12:58:34 -0600

    Include platform-utils repo alongside csm-testing and goss-servers rpm

--- lib/install-goss-tests.sh
+++ lib/install-goss-tests.sh
@@ -24,18 +24,20 @@ if [ -f /etc/pit-release ]; then
     fi
 
     NCNS=$(grep -oE "($MTOKEN|$STOKEN|$WTOKEN)" /etc/dnsmasq.d/statics.conf | grep -v m001 | sort -u)
-    CMS_TESTING_RPM=$(find $RPMDIR/csm-testing* | sort -V | tail -1)
-    GOSS_SERVERS_RPM=$(find $RPMDIR/goss-servers* | sort -V | tail -1)
+    CMS_TESTING_RPM=$(find $RPMDIR/csm-testing-* | sort -V | tail -1)
+    GOSS_SERVERS_RPM=$(find $RPMDIR/goss-servers-* | sort -V | tail -1)
+    PLATFORM_UTILS_RPM=$(find $RPMDIR/platform-utils-* | sort -V | tail -1)
 
     for ncn in $NCNS; do
-        scp $CMS_TESTING_RPM $GOSS_SERVERS_RPM $ncn:/tmp/
+        scp $CMS_TESTING_RPM $GOSS_SERVERS_RPM $PLATFORM_UTILS_RPM $ncn:/tmp/
         # shellcheck disable=SC2029
-        ssh $ncn "rpm -Uvh --force /tmp/$(basename $CMS_TESTING_RPM) /tmp/$(basename $GOSS_SERVERS_RPM) && systemctl restart goss-servers"
+        ssh $ncn "rpm -Uvh --force /tmp/$(basename $CMS_TESTING_RPM) /tmp/$(basename $GOSS_SERVERS_RPM) /tmp/$(basename $PLATFORM_UTILS_RPM) && systemctl restart goss-servers"
     done
 
     # The rpms should have been installed on the pit at the same time csi was installed. Trust, but verify:
-    rpm -qa | grep goss-servers || (zypper install $GOSS_SERVERS_RPM && systemctl enable goss-servers && systemctl restart goss-servers)
-    rpm -qa | grep csm-testing || zypper install $CMS_TESTING_RPM
+    rpm -qa | grep goss-servers- || (zypper install $GOSS_SERVERS_RPM && systemctl enable goss-servers && systemctl restart goss-servers)
+    rpm -qa | grep csm-testing- || zypper install $CMS_TESTING_RPM
+    rpm -qa | grep platform-utils- || zypper install $PLATFORM_UTILS_RPM
 else
     echo "ERROR: This script should only be run from the pit node prior to the handoff and reboot into ncn-m001"
     exit 1
