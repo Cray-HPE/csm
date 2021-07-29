73884a206f0f8fa56df0840681b2c12546f07cc7
Dat Le <dle@cray.com>
2021-07-29 14:35:40 -0500

    fix unbound_psp_check funtion to match 0.9.4 upgrade version

--- upgrade.sh
+++ upgrade.sh
@@ -39,12 +39,17 @@ function undeploy() {
 
 # Check for manually create unbound PSP that is not managed by helm
 function unbound_psp_check() {
-    unbound_psp="$(kubectl get ClusterRoleBinding -n services cray-dns-unbound-psp -o yaml |grep helm |wc -l)"||true
-    if [[ "$unbound_psp" -eq "0" ]]; then
-        echo "Found ClusterRoleBinding cray-dns-unbound-psp NOT managed by helm"
-        kubectl delete ClusterRoleBinding -n services cray-dns-unbound-psp
-        echo "Delete ClusterRoleBinding cray-dns-unbound-psp"
+    echo "Checking for manually created cray-unbound-coredns-psp"
+    unbound_psp_exist="$(kubectl get ClusterRoleBinding -n services |grep cray-unbound-coredns-psp |wc -l)"||true
+    if [[ "$unbound_psp_exist" -eq "1" ]]; then
+        unbound_psp_helm_check="$(kubectl get ClusterRoleBinding -n services cray-unbound-coredns-psp -o yaml |grep helm |wc -l)"||true
+        if [[ "$unbound_psp_helm_check" -eq "0" ]]; then
+            echo "Found ClusterRoleBinding cray-dns-unbound-psp NOT managed by helm"
+            kubectl delete ClusterRoleBinding -n services cray-unbound-coredns-psp
+            echo "Delete ClusterRoleBinding cray-dns-unbound-psp"
+        fi
     fi
+    echo "cray-unbound-coredns-psp check Done"
 }
 
 # Deploy services critical for Nexus to run
