6e677df1d06043c9dea56463103f2fe1d20c4e43
Eric Lund <eric.lund@hpe.com>
2021-08-24 07:46:39 -0500

    CASMCLOUD-1189 switch cloud and user artifacts to algol60 source

--- docker/transform.sh
+++ docker/transform.sh
@@ -57,7 +57,7 @@ DISTDIR=$1
     cp -r baseos/alpine:3.12 baseos/alpine:3.12.0
     cp -r cray/cray-nexus-setup:0.5.2 cray/cray-nexus-setup:0.3.2
     cp -r cray/cray-nexus-setup:0.5.2 cray/cray-nexus-setup:0.4.0
-    cp -r cray/cray-uai-broker:1.2.1 cray/cray-uai-broker:latest
+    cp -r cray/cray-uai-broker:1.2.3 cray/cray-uai-broker:latest
     cp -r loftsman/docker-kubectl:0.2.0 loftsman/docker-kubectl:latest
     cp -r loftsman/loftsman:0.5.1 loftsman/loftsman:latest
     cp -r openpolicyagent/opa:0.24.0-envoy-1 openpolicyagent/opa:latest
