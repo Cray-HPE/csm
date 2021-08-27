672027f03043fb125fc2f23423e5d740e41f29f0
Russell Bunch <rbunch@cray.com>
2021-08-27 18:09:12 -0500

    Use new 1.2 assets

--- assets.sh
+++ assets.sh
@@ -3,21 +3,21 @@
 # Copyright 2021 Hewlett Packard Enterprise Development LP
 
 PIT_ASSETS=(
-    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/release/csm-1.0/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.14-20210803183048-g592db0a.iso
-    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/release/csm-1.0/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.14-20210803183048-g592db0a.packages
-    http://car.dev.cray.com/artifactory/csm/MTL/sle15_sp2_ncn/x86_64/release/csm-1.0/casmpet-team/cray-pre-install-toolkit-sle15sp2.x86_64-1.4.14-20210803183048-g592db0a.verified
+    http://arti.dev.cray.com/artifactory/csm-misc-master-local/dev/master/sle15_sp3_ncn/x86_64/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.0-20210827082307-g1784709.iso
+    http://arti.dev.cray.com/artifactory/csm-misc-master-local/dev/master/sle15_sp3_ncn/x86_64/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.0-20210827082307-g1784709.packages
+    http://arti.dev.cray.com/artifactory/csm-misc-master-local/dev/master/sle15_sp3_ncn/x86_64/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.0-20210827082307-g1784709.verified
 )
 
 KUBERNETES_ASSETS=(
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.1.73/kubernetes-0.1.73.squashfs
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.1.73/5.3.18-24.75-default-0.1.73.kernel
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.1.73/initrd.img-0.1.73.xz
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.2.1/kubernetes-0.2.1.squashfs
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.2.1/5.3.18-59.16-default-0.2.1.kernel
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/kubernetes/0.2.1/initrd.img-0.2.1.xz
 )
 
 STORAGE_CEPH_ASSETS=(
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.1.70/storage-ceph-0.1.70.squashfs
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.1.70/5.3.18-24.75-default-0.1.70.kernel
-    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.1.70/initrd.img-0.1.70.xz
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.2.1/storage-ceph-0.2.1.squashfs
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.2.1/5.3.18-59.16-default-0.2.1.kernel
+    https://arti.dev.cray.com/artifactory/node-images-stable-local/shasta/storage-ceph/0.2.1/initrd.img-0.2.1.xz
 )
 
 FIRMWARE_PACKAGE=http://car.dev.cray.com/artifactory/internal/~PVIRTUCIO/release/Cray_Firmware/03.04.2021_v1/firmware_package_03042021a.tgz
