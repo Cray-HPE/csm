780d8494ef9986e1af8a1c1e014c76a6b1d8bf84
kimjensen-hpe <kim.jensen@hpe.com>
2021-09-07 10:47:22 -0600

    CASMINST-2998-1.1 : BREAK/FIX: surtur csm-1.0.0-beta.73: sysmgmt-health pods in IPBO

--- docker/transform.sh
+++ docker/transform.sh
@@ -42,6 +42,7 @@ DISTDIR=$1
     mv -v vault:* library/
     mv -v docker.io/prom/* prom/
     mv -v quay.io/prometheus/* prometheus/
+    mv -v docker.io/jimmidyson/ jimmidyson/
     mv -v docker.io/weaveworks/ weaveworks/
     mv -v docker.io/unguiculus/ unguiculus/
     mv -v gcr.io/spiffe-io/ spiffe-io/
