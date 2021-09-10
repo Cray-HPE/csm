e0620dcd1612d5136cc5a4a4d2bb81ddfe6c3854
Zach Crisler <zachary.crisler@hpe.com>
2021-09-10 14:35:35 -0400

    Finds docs-csm and csm-install-workarounds RPMs when building a release

--- release.sh
+++ release.sh
@@ -123,18 +123,11 @@ createrepo "${BUILDDIR}/rpm/cray/csm/sle-15sp1"
 createrepo "${BUILDDIR}/rpm/cray/csm/sle-15sp1-compute"
 createrepo "${BUILDDIR}/rpm/cray/csm/sle-15sp2"
 
-# In case one of the following RPM clauses fails, let's first do a search for these RPMs.
-# That way if their locations have mysteriously moved and one of these sections fails, we
-# will at least know where the RPM is actually located (or if it is missing)
-DOCS_RPM_PREFIX="docs-csm"
-CIW_RPM_PREFIX="csm-install-workarounds"
-find "${BUILDDIR}"/rpm -type f \( -name "${DOCS_RPM_PREFIX}-"\*.rpm -o -name "${CIW_RPM_PREFIX}-"\*.rpm \) -print
-
 # Extract docs RPM into release
 mkdir -p "${BUILDDIR}/tmp/docs"
 (
     cd "${BUILDDIR}/tmp/docs"
-    rpm2cpio "${BUILDDIR}/rpm/cray/csm/sle-15sp2/sle-15sp2/docs-csm/noarch/${DOCS_RPM_PREFIX}-"*.rpm | cpio -idvm ./usr/share/doc/metal/*
+    find "${BUILDDIR}/rpm/cray/csm/sle-15sp2" -type f -name docs-csm-\*.rpm | head -n 1 | xargs -n 1 rpm2cpio | cpio -idvm ./usr/share/doc/metal/*
 )
 mv "${BUILDDIR}/tmp/docs/usr/share/doc/metal" "${BUILDDIR}/docs"
 
@@ -142,7 +135,7 @@ mv "${BUILDDIR}/tmp/docs/usr/share/doc/metal" "${BUILDDIR}/docs"
 mkdir -p "${BUILDDIR}/tmp/wars"
 (
     cd "${BUILDDIR}/tmp/wars"
-    rpm2cpio "${BUILDDIR}/rpm/cray/csm/sle-15sp2/${CIW_RPM_PREFIX}-"*.rpm | cpio -idmv ./opt/cray/csm/workarounds/*
+    find "${BUILDDIR}/rpm/cray/csm/sle-15sp2" -type f -name csm-install-workarounds-\*.rpm | head -n 1 | xargs -n 1 rpm2cpio | cpio -idvm ./opt/cray/csm/workarounds/*
     find . -type f -name '.keep' -delete
 )
 mv "${BUILDDIR}/tmp/wars/opt/cray/csm/workarounds" "${BUILDDIR}/workarounds"
