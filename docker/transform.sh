1fb611549aabe4c6c5d5e0134b03dc5e001fb816
Di Zou <ahoy@algol60.net>
2021-09-10 15:28:10 -0400

    spilo fix for 0.9

--- docker/transform.sh
+++ docker/transform.sh
@@ -20,6 +20,7 @@ DISTDIR=$1
     mv arti.dev.cray.com/wlm-slurm-docker-stable-local/* dtr.dev.cray.com/cray/
     mv arti.dev.cray.com/internal-docker-stable-local/* dtr.dev.cray.com/cray/
     mv arti.dev.cray.com/csm-docker-unstable-local/docker.io/library/* dtr.dev.cray.com/library/
+    mv -v artifactory.algol60.net/csm-docker/stable/registry.opensource.zalan.do/acid dtr.dev.cray.com/acid/
     mv artifactory.algol60.net/csm-docker/stable/* dtr.dev.cray.com/cray/
 
     cd dtr.dev.cray.com
