6f38ed2d8f8329b728ec4af904bb3821a4dcdffd
Pete Halseth <peter.halseth@hpe.com>
2021-10-04 18:29:12 -0500

    transform updates for gitea and kiali

--- docker/transform.sh
+++ docker/transform.sh
@@ -56,6 +56,8 @@ DISTDIR=$1
     mv -v docker.io/istio istio/
     mv -v docker.io/jaegertracing jaegertracing/
     mv -v docker.io/jboss/ jboss/
+    mv -v docker.io/gitea/gitea* docker.io/
+    mv -v docker.io/kiali/kiali* docker.io/
     mv -v docker.io/jettech jettech/
     mv -v docker.io/jimmidyson/ jimmidyson/
     mv -v docker.io/kiwigrid/ kiwigrid/
