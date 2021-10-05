2a662643e86806d8f79631f1d89a6ac3280f8f34
Pete Halseth <peter.halseth@hpe.com>
2021-10-04 19:27:28 -0500

    update assets.sh to use stable pit images

--- assets.sh
+++ assets.sh
@@ -3,9 +3,9 @@
 # Copyright 2021 Hewlett Packard Enterprise Development LP
 
 PIT_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/unstable/cray-pre-install-toolkit/1.5.2/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.2-20211004225216-gd77714e.iso
-    https://artifactory.algol60.net/artifactory/csm-images/unstable/cray-pre-install-toolkit/1.5.2/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.2-20211004225216-gd77714e.packages
-    https://artifactory.algol60.net/artifactory/csm-images/unstable/cray-pre-install-toolkit/1.5.2/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.2-20211004225216-gd77714e.verified
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.2/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.2-20211004225216-gd77714e.iso
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.2/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.2-20211004225216-gd77714e.packages
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.2/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.2-20211004225216-gd77714e.verified
 )
 
 KUBERNETES_ASSETS=(
