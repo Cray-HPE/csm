5a7aa361157b6b5c5b7b26b323df34d7d3abe0d2
rustydb <doomslayer@hpe.com>
2021-10-05 01:22:22 -0500

    pit-init is now installed by csm-rpms, instead of the autoinst.xml file (which was used as an interim while pit-init landed in csm-rpms)

--- assets.sh
+++ assets.sh
@@ -3,9 +3,9 @@
 # Copyright 2021 Hewlett Packard Enterprise Development LP
 
 PIT_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.2/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.2-20211004225216-gd77714e.iso
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.2/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.2-20211004225216-gd77714e.packages
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.2/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.2-20211004225216-gd77714e.verified
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.3/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.3-20211005052915-ga74ab3a.iso
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.3/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.3-20211005052915-ga74ab3a.packages
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.3/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.3-20211005052915-ga74ab3a.verified
 )
 
 KUBERNETES_ASSETS=(
