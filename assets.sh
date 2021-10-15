dd92977d154ad87422260c28cc7983d8fa96afb2
John Heemstra <heemstra@hpe.com>
2021-10-15 17:17:41 -0400

    CASMREL-788: align kernels
    The NCNs got a newer kernel and this was not reflected in the previous PR.
    
    The LiveCD was also rebuilt to pick up the newer, matching kernel.

--- assets.sh
+++ assets.sh
@@ -3,20 +3,20 @@
 # Copyright 2021 Hewlett Packard Enterprise Development LP
 
 PIT_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.5-20211015182608-g8644881.iso
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.5-20211015182608-g8644881.packages
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.5-20211015182608-g8644881.verified
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.5-20211015205247-g8644881.iso
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.5-20211015205247-g8644881.packages
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.5.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.5.5-20211015205247-g8644881.verified
 )
 
 KUBERNETES_ASSETS=(
     https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.7/kubernetes-0.2.7.squashfs
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.7/5.3.18-59.24-default-0.2.7.kernel
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.7/5.3.18-59.27-default-0.2.7.kernel
     https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.7/initrd.img-0.2.7.xz
 )
 
 STORAGE_CEPH_ASSETS=(
     https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.7/storage-ceph-0.2.7.squashfs
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.7/5.3.18-59.24-default-0.2.7.kernel
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.7/5.3.18-59.27-default-0.2.7.kernel
     https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.7/initrd.img-0.2.7.xz
 )
 
