c70b13dc7d7d5f00161b9008a67eb49eb6c6fe69
Zach Crisler <zachary.crisler@hpe.com>
2021-11-18 13:30:14 -0500

    Squashed 'vendor/stash.us.cray.com/scm/shastarelm/release/' changes from 58c5b289..32c51bdf
    32c51bdf Pass required argument to get-pyyaml function; Do not assume -e or -x are set
    37c862dc Pin skopeo image to v1.4.1
    REVERT: 58c5b289 skopeo-sync uploads images fully scoped to upstream registries
    
    git-subtree-dir: vendor/stash.us.cray.com/scm/shastarelm/release
    git-subtree-split: 32c51bdff30a6ea07a35bf53d4222b9cfa1daa39

--- lib/release.sh
+++ lib/release.sh
@@ -4,7 +4,7 @@
 
 : "${PACKAGING_TOOLS_IMAGE:=arti.dev.cray.com/internal-docker-stable-local/packaging-tools:0.11.0}"
 : "${RPM_TOOLS_IMAGE:=arti.dev.cray.com/internal-docker-stable-local/rpm-tools:1.0.0}"
-: "${SKOPEO_IMAGE:=quay.io/skopeo/stable:latest}"
+: "${SKOPEO_IMAGE:=quay.io/skopeo/stable:v1.4.1}"
 : "${CRAY_NEXUS_SETUP_IMAGE:=arti.dev.cray.com/csm-docker-stable-local/cray-nexus-setup:0.5.2}"
 
 # Prefer to use docker, but for environments with podman
@@ -52,6 +52,19 @@ function cmd_retry() {
     done
     echo "PROGRAMMING LOGIC ERROR: This line should never be reached" 1>&2
     exit 1
+}
+
+# usage: run_cmd <command> [arg1] [arg2] ...
+# Runs the command. On success, just returns 0.
+# On fail, prints an appropriate error message, returns 1
+function run_cmd
+{
+    local -i rc
+    rc=0
+    "$@" || rc=$?
+    [ $rc -eq 0 ] && return 0
+    echo "ERROR: Command failed with return code $rc: $*" 1>&2
+    return 1
 }
 
 # usage: generate-nexus-config (blobstore|repository) FILE
@@ -197,17 +210,31 @@ function update-index-yaml() {
 
 # usage: get-pyyaml TARGET_DIRECTORY
 function get-pyyaml() {
+    # If we can import yaml, then nothing to do
+    if python3 -c "import yaml" ; then
+        echo "PyYAML appears to be installed"
+        return 0
+    fi
+    echo "PyYAML does not appear to be installed. Installing it to '$1'"
     python3 -m ensurepip || true
-    pip3 install PyYAML \
-        --no-cache-dir \
-        --trusted-host arti.dev.cray.com \
-        --index-url https://arti.dev.cray.com:443/artifactory/api/pypi/pypi-remote/simple \
-        --ignore-installed \
-        --target="$1" \
-        --upgrade
-    export PYTHONPATH="${PYTHONPATH}:$1"
+    run_cmd pip3 install PyYAML \
+            --no-cache-dir \
+            --trusted-host arti.dev.cray.com \
+            --index-url https://arti.dev.cray.com:443/artifactory/api/pypi/pypi-remote/simple \
+            --ignore-installed \
+            --target="$1" \
+            --upgrade || return 1
+    if [[ -n ${PYTHONPATH} ]]; then
+        export PYTHONPATH="${PYTHONPATH}:$1"
+    else
+        export PYTHONPATH="$1"
+    fi
     # Make sure we can import it
-    python3 -c "import yaml"
+    if python3 -c "import yaml" ; then
+        return 0
+    fi
+    echo "ERROR: Cannot import PyYAML even after pip installing it" 1>&2
+    return 1
 }
 
 # usage: print-time NUM_SECONDS
@@ -253,8 +280,9 @@ function skopeo-sync() {
     local pymod_dir="${tmpdir}/pymod"
     local tmp_index="${tmpdir}/index"
 
-    cp "$1" "${orig_index}"
-    mkdir -p "$destdir" "$tmpdir"
+    # We don't know if we're being called with set -e or not, so best to play it safe
+    run_cmd cp -v "$1" "${orig_index}" || return 1
+    run_cmd mkdir -pv "$destdir" "$tmpdir" "${pymod_dir}" || return 1
     
     # Normally I would use let for arithmetic, but if the let expression evaluates to 0,
     # the return code is non-0, which breaks us if we're operating under set -e
@@ -265,15 +293,15 @@ function skopeo-sync() {
     echo "skopeo-sync: end_time_seconds=${end_time_seconds}"
 
     # Make sure we've got PyYAML
-    get-pyyaml
+    get-pyyaml "${pymod_dir}"
 
     # Pre-process the index file through our Python script, just so that when we later do diffs,
     # it is comparing apples to apples, so to speak
-    update-index-yaml "${orig_index}"
-    cp "${orig_index}" "$index"
+    update-index-yaml "${orig_index}" || return 1
+    run_cmd cp -v "${orig_index}" "$index" || return 1
 
     #DEBUG
-    cat "$index"
+    run_cmd cat "$index" || return 1
 
     while [ true ]; do
         echo "$(date) skopeo-sync: Beginning attempt #${attempt_number}"
@@ -314,35 +342,37 @@ function skopeo-sync() {
         let attempt_number+=1
 
         echo "skopeo-sync: Cleaning up incomplete images"
-        find "${destdir}" -type d -name \*:\* ! -exec bash -c "[[ -f {}/manifest.json ]]" \; -print -exec rm -rf {} \; -prune
+        find "${destdir}" -type d -name \*:\* ! -exec bash -c "[[ -f {}/manifest.json ]]" \; -print -exec rm -rvf {} \; -prune || return 1
 
         # For reasons I have not yet figured out, the job always tries to sync alpine:3.12
         # So it has to be removed if we are going to retry
         echo "skopeo-sync: Cleaning up alpine:3.12"
-        find "${destdir}" -type d -name "alpine:3.12" -print -exec rm -rf {} \; -prune
+        find "${destdir}" -type d -name "alpine:3.12" -print -exec rm -rvf {} \; -prune || return 1
 
         # Remove any completed images from index.yaml
-        find "${destdir}" -type d -name \*:\* -print > "${completed_image_file}"
-        if [ -s "${completed_image_file}" ]; then
+        if ! find "${destdir}" -type d -name \*:\* -print > "${completed_image_file}" ; then
+            echo "ERROR searching for completed images or writing to '${completed_image_file}'" 1>&2
+            return 1
+        elif [ -s "${completed_image_file}" ]; then
             total_synced=$(wc -l "${completed_image_file}" | awk '{ print $1 }')
             new_synced=$((${total_synced} - ${previously_synced}))
             previously_synced=${total_synced}
             echo "skopeo-sync: Found ${new_synced} new completely synced images"
 
             # Strip off the leading ${destdir}/ from the paths
-            sed -i "s#^${destdir}/##" "${completed_image_file}"
-            cp "$index" "${tmp_index}"
+            run_cmd sed -i "s#^${destdir}/##" "${completed_image_file}" || return 1
+            run_cmd cp -v "$index" "${tmp_index}" || return 1
             
             # DEBUG
-            cat "$index"
-            cat "${orig_index}"
-            cat "${completed_image_file}"
+            run_cmd cat "$index" || return 1
+            run_cmd cat "${orig_index}" || return 1
+            run_cmd cat "${completed_image_file}" || return 1
 
             update-index-yaml "$index" "${orig_index}" "${completed_image_file}"
             diff "${tmp_index}" "$index" || true
 
             # DEBUG
-            cat "${completed_image_file}"
+            run_cmd cat "${completed_image_file}" || return 1
 
             # For reasons I have not yet figured out, some images are synced which do not appear to be listed
             # in the manifest. It may be a dependency of some kind, or perhaps something to do with "latest".
@@ -351,8 +381,8 @@ function skopeo-sync() {
             if [ -s "${completed_image_file}" ]; then
                 grep -E "^[^[:space:]]" "${completed_image_file}" | sed "s#^#${destdir}/#" > "${completed_image_file}"
                 #DEBUG
-                cat "${completed_image_file}"
-                cat "${completed_image_file}" | xargs -r -t rm -rf
+                run_cmd cat "${completed_image_file}" || return 1
+                cat "${completed_image_file}" | xargs -r -t rm -rvf
             fi
         fi
 
@@ -362,10 +392,10 @@ function skopeo-sync() {
 
     # It gives me a warm feeling to clean up after myself
     if [ -f "${index}" ]; then
-        rm -f "${index}"
+        run_cmd rm -vf "${index}" || return 1
     fi
     if [ -d "${tmpdir}" ]; then
-        rm -rf "${tmpdir}"
+        run_cmd rm -rvf "${tmpdir}" || return 1
     fi
     
     return ${function_rc}
