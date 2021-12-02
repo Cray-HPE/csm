c3ef5efc99678de297bf17b68e5928700cac9ccf
rustydb <doomslayer@hpe.com>
2021-12-02 15:56:41 -0600

    New NCN Images
    CASMINST-3653

--- assets.sh
+++ assets.sh
@@ -9,15 +9,15 @@ PIT_ASSETS=(
 )
 
 KUBERNETES_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.28/kubernetes-0.2.28.squashfs
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.28/5.3.18-59.19-default-0.2.28.kernel
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.28/initrd.img-0.2.28.xz
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.29/kubernetes-0.2.29.squashfs
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.29/5.3.18-59.19-default-0.2.29.kernel
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.2.29/initrd.img-0.2.29.xz
 )
 
 STORAGE_CEPH_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.28/storage-ceph-0.2.28.squashfs
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.28/5.3.18-59.19-default-0.2.28.kernel
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.28/initrd.img-0.2.28.xz
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.29/storage-ceph-0.2.29.squashfs
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.29/5.3.18-59.19-default-0.2.29.kernel
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.2.29/initrd.img-0.2.29.xz
 )
 
 HPE_SIGNING_KEY=https://arti.dev.cray.com/artifactory/dst-misc-stable-local/SigningKeys/HPE-SHASTA-RPM-PROD.asc
