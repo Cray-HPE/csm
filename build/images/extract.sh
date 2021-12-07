6c7a0b555c86d8b71a9d76b39712168270d321a3
Zach Crisler <zachary.crisler@hpe.com>
2021-12-07 12:10:56 -0500

    Refactor extract.sh to support filtering for specific charts

--- build/images/extract.sh
+++ build/images/extract.sh
@@ -14,7 +14,7 @@ function extract-repos() {
 
 function extract-charts() {
     docker run --rm -i "$YQ_IMAGE" e -N -o json '.spec.charts' - < "$1" \
-    | jq -r '.[] | (.source) + "\t" + (.name) + "\t" + (.version) + "\t" + (.values | [paths(scalars) as $path | {"key": $path | join("."), "value": getpath($path)}] | map("\(.key)=\(.value|tostring)") | join(","))'
+    | jq -r '.[] | (.releaseName // .name) + "\t" + (.source) + "\t" + (.name) + "\t" + (.version) + "\t" + (.values | [paths(scalars) as $path | {"key": $path | join("."), "value": getpath($path)}] | map("\(.key)=\(.value|tostring)") | join(","))'
 }
 
 function get-customizations() {
@@ -40,26 +40,44 @@ function extract-images() {
     } | docker run --rm -i "$YQ_IMAGE" e -N '.. | .image? | select(.)' - | sort -u | sed -e '/^image: null$/d' -e '/^type: string$/d' | tee >(cat -n 1>&2)
 }
 
-[[ $# -eq 0 ]] && set -- "${ROOTDIR}/manifests"/*.yaml
 
-while [[ $# -gt 0 ]]; do
-    manifest="$1"
-    shift
+function usage() {
+    echo >&2 "usage: ${0##*/} MANIFEST [CHART ...]"
+    exit 1
+}
 
-    echo >&2 "+ $manifest"
+[[ $# -gt 0 ]] || usage
 
-    # clean up existing repos
-    #helm repo list -o yaml | docker run --rm -i "$YQ_IMAGE" e '.[] | .name' - | xargs --verbose -n 1 helm repo remove
+manifest="$1"
+shift
 
-    # Update helm repos
-    extract-repos "$manifest" | while read name url; do
-        echo >&2 "+ helm repo add $name $url"
-        helm repo add --force-update "$name" "$url" >&2
-        helm repo update --fail-on-repo-update-fail "$name" >&2
-    done
+if [[ $# -gt 0 ]]; then
+    function filter-releases() {
+        local e match="$1"
+        shift
+        for e; do [[ "$e" == "$match" ]] && return 0; done
+        return 1
+    }
+else
+    function filter-releases() {
+        return 0
+    }
+fi
 
-    # extract images from chart
-    extract-charts "$manifest" | while read repo chart version values; do
-        extract-images "$repo" "$chart" "$version" "$values"
-    done
+echo >&2 "+ $manifest"
+
+# clean up existing repos
+#helm repo list -o yaml | docker run --rm -i "$YQ_IMAGE" e '.[] | .name' - | xargs --verbose -n 1 helm repo remove
+
+# Update helm repos
+extract-repos "$manifest" | while read name url; do
+    echo >&2 "+ helm repo add $name $url"
+    helm repo add --force-update "$name" "$url" >&2
+    helm repo update --fail-on-repo-update-fail "$name" >&2
+done
+
+# extract images from chart
+extract-charts "$manifest" | while read release repo chart version values; do
+    filter-releases "$chart" "$@" || continue
+    extract-images "$repo" "$chart" "$version" "$values"
 done | sort -u
