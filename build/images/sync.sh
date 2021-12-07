cf67ac44203eadd9031277b4bb24e6cfd1e62e11
Zach Crisler <zachary.crisler@hpe.com>
2021-12-07 12:57:28 -0500

    Sync images based on upstream repo

--- build/images/sync.sh
+++ build/images/sync.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-: "${SKOPEO_IMAGE:=quay.io/skopeo/stable:v1.4.1}"
+: "${SKOPEO_IMAGE:=artifactory.algol60.net/quay.io/skopeo/stable:v1.4.1}"
 
 set -euo pipefail
+
+function get-repo-upstream() {
+    echo "$1" | sed \
+        -e 's/^artifactory.algol60.net\/docker.io\//docker\.io\//' \
+        -e 's/^artifactory.algol60.net\/gcr.io\//gcr\.io\//' \
+        -e 's/^artifactory.algol60.net\/k8s.gcr.io\//k8s\.gcr\.io\//' \
+        -e 's/^artifactory.algol60.net\/quay.io\//quay\.io\//'
+}
 
 [[ $# -gt 0 ]] || {
     echo >&2 "usage: ${0##*/} DIR IMAGE ..."
@@ -13,17 +21,19 @@ destdir="$1"
 shift
 
 while [[ $# -gt 0 ]]; do
-    image="$1"
+    imgsrc="$1"
     shift
 
-    echo >&2 "+ skopeo copy $image"
+    echo >&2 "+ skopeo copy $imgsrc"
 
-    if [[ -e "${destdir}/${image}" ]]; then
-        echo >&2 "error: File exists: ${destdir}/${image}"
+    imgdest="$(get-repo-upstream "$imgsrc")"
+
+    if [[ -e "${destdir}/${imgdest}" ]]; then
+        echo >&2 "error: File exists: ${destdir}/${imgdest}"
         exit 255
     fi
 
-    mkdir -p "${destdir}/${image}"
+    mkdir -p "${destdir}/${imgdest}"
 
     # Copy image
     docker run --rm \
@@ -31,5 +41,5 @@ while [[ $# -gt 0 ]]; do
         -v "$(realpath -m "$destdir"):/data" \
         "$SKOPEO_IMAGE" \
         --override-os linux --override-arch amd64 \
-        copy --retry-times 5 --all "docker://$image" "dir:/data/$image" >&2 || exit 255
+        copy --retry-times 5 --all "docker://$imgsrc" "dir:/data/$imgdest" >&2 || exit 255
 done
