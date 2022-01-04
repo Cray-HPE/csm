ef0a72f8a1c31bdb87ef3981a30ba173bbfe3750
Pete Halseth <peter.halseth@hpe.com>
2022-01-04 13:03:23 -0600

    update spp repo URLs in gen-rpm-index

--- hack/gen-rpm-index.sh
+++ hack/gen-rpm-index.sh
@@ -109,6 +109,6 @@ docker run --rm -i arti.dev.cray.com/internal-docker-stable-local/packaging-tool
 -d  https://artifactory.algol60.net/artifactory/csm-rpms/hpe/unstable/                              cray/csm/sle-15sp2 \
 -d  https://artifactory.algol60.net/artifactory/csm-rpms/hpe/unstable/sle-15sp2                              cray/csm/sle-15sp2 \
 -d  https://artifactory.algol60.net/artifactory/csm-rpms/hpe/stable/sle-15sp2/                               cray/csm/sle-15sp2 \
--d  https://downloads.linux.hpe.com/SDR/repo/spp/SUSE_LINUX/                                                 hpe/spp \
--d  https://downloads.linux.hpe.com/SDR/repo/mlnx_ofed_cx4plus/SUSE_LINUX/                                   hpe/mlnx_ofed_cx4plus \
+-d  https://downloads.linux.hpe.com/SDR/repo/spp/SUSE_LINUX/SLES15-SP2/x86_64/current/                       hpe/spp/sle-15sp2/current \
+-d  https://downloads.linux.hpe.com/SDR/repo/mlnx_ofed_cx4plus/SUSE_LINUX/SLES15-SP2/x86_64/5.2-1.0.4.0/         hpe/mlnx_ofed_cx4plus/ \
     -
