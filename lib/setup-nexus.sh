7fb13468cc90227ce9f6dff89e519a50c809b604
Zach Crisler <zachary.crisler@hpe.com>
2022-03-28 16:22:35 -0400

    CASMINST-4363: skopeo-sync uploads images from docker/dtr.dev.cray.com to registry.local

--- lib/setup-nexus.sh
+++ lib/setup-nexus.sh
@@ -56,7 +56,7 @@ nexus-setup blobstores   "${ROOTDIR}/nexus-blobstores.yaml" "$unbound_ip"
 nexus-setup repositories "${ROOTDIR}/nexus-repositories.yaml" "$unbound_ip"
 
 # Upload assets to existing repositories
-skopeo-sync "${ROOTDIR}/docker" "$unbound_ip"
+skopeo-sync "${ROOTDIR}/docker/dtr.dev.cray.com" "$unbound_ip"
 nexus-upload helm "${ROOTDIR}/helm" "${CHARTS_REPO:-"charts"}" "$unbound_ip"
 
 # Upload repository contents
