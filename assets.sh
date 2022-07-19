e64fec37d318e7029e8fedbfe138a59e9110461d
Pete Halseth <peter.halseth@hpe.com>
2022-07-18 20:01:06 -0500

    update pit image version to *933, docs bump to v1.3.11

--- assets.sh
+++ assets.sh
@@ -23,9 +23,9 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 #
 PIT_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.3/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.3-20220718191929.iso
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.3/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.3-20220718191929.packages
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.3/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.3-20220718191929.verified
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.3/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.3-20220718191933.iso
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.3/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.3-20220718191933.packages
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.3/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.3-20220718191933.verified
 )
 
 KUBERNETES_ASSETS=(
