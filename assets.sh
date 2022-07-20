0f9b5941c46170784a5e1b98716b4585c8bedc16
Russell Bunch <doomslayer@hpe.com>
2022-07-20 13:45:00 -0500

    CASMREL-1030
    LiveCD:
    - CASMINST-5026
    - CASMINST-4932
    
    NCNs:
    - CASMHMS-5606
    - CASMINST-5026
    - CASMINST-5034
    - CASMINST-5037
    - MTL-1853

--- assets.sh
+++ assets.sh
@@ -23,21 +23,21 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 #
 PIT_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.5-20220719215856.iso
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.5-20220719215856.packages
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.5-20220719215856.verified
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.5-20220720184232.iso
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.5-20220720184232.packages
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.5/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.5-20220720184232.verified
 )
 
 KUBERNETES_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.3.22/kubernetes-0.3.22.squashfs
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.3.22/5.3.18-150300.59.76-default-0.3.22.kernel
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.3.22/initrd.img-0.3.22.xz
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.3.23/kubernetes-0.3.23.squashfs
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.3.23/5.3.18-150300.59.76-default-0.3.23.kernel
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.3.23/initrd.img-0.3.23.xz
 )
 
 STORAGE_CEPH_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.3.22/storage-ceph-0.3.22.squashfs
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.3.22/5.3.18-150300.59.76-default-0.3.22.kernel
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.3.22/initrd.img-0.3.22.xz
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.3.23/storage-ceph-0.3.23.squashfs
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.3.23/5.3.18-150300.59.76-default-0.3.23.kernel
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.3.23/initrd.img-0.3.23.xz
 )
 
 HPE_SIGNING_KEY=https://arti.dev.cray.com/artifactory/dst-misc-stable-local/SigningKeys/HPE-SHASTA-RPM-PROD.asc
