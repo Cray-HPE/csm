9818d1f40fd5841ed9ab2abd9e157ca7f693caa4
Russell Bunch <doomslayer@hpe.com>
2022-09-12 13:49:38 -0500

    MTL-1952 MTL-1953 Fixes for `pit-init`
    Fixes usage for `csi-setup-lan0.sh`. Also fixes the `PITDATA` variable
    in `pit-init.sh` for systems running in memory (e.g. for recovery
    where no partition can be provided).

--- assets.sh
+++ assets.sh
@@ -23,9 +23,9 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 #
 PIT_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.8/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.8-20220907202418.iso
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.8/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.8-20220907202418.packages
-    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.8/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.8-20220907202418.verified
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.8/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.8-20220912184913.iso
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.8/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.8-20220912184913.packages
+    https://artifactory.algol60.net/artifactory/csm-images/stable/cray-pre-install-toolkit/1.8.8/cray-pre-install-toolkit-sle15sp3.x86_64-1.8.8-20220912184913.verified
 )
 
 KUBERNETES_ASSETS=(
