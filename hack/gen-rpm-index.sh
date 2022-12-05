e8ba6e71de2911a9308e48e9614903b39616c8b9
Pete Halseth <peter.halseth@hpe.com>
2022-12-05 16:32:18 -0600

    add cos-rpm-stable-local repo to rpm index

--- hack/gen-rpm-index.sh
+++ hack/gen-rpm-index.sh
@@ -236,4 +236,5 @@ docker run ${REPO_CREDS_DOCKER_OPTIONS} --rm -i arti.hpc.amslabs.hpecorp.net/int
     -d  https://artifactory.algol60.net/artifactory/sles-mirror/Updates/SLE-Product-SLES/15-SP2-LTSS/x86_64/update_debug  cray/csm/sle-15sp2 \
     -d  https://artifactory.algol60.net/artifactory/sles-mirror/Updates/SLE-Module-Server-Applications/15-SP3/x86_64/update/  cray/csm/sle-15sp2 \
     -d  https://artifactory.algol60.net/artifactory/sles-mirror/Updates/SLE-Module-Server-Applications/15-SP2/x86_64/update/  cray/csm/sle-15sp2 \
+    -d  https://artifactory.algol60.net/artifactory/dst-rpm-mirror/cos-rpm-stable-local/release/cos-2.4/sle15_sp3_ncn/  cray/csm/sle-15sp3 \
     -
