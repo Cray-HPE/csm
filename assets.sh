561cf475c9d8ce01500d68126d43bb97b931b92c
John Heemstra <madhatter@hpe.com>
2022-12-05 15:42:41 -0500

    CASMTRIAGE-4656: NCN images

--- assets.sh
+++ assets.sh
@@ -29,15 +29,15 @@ PIT_ASSETS=(
 )
 
 KUBERNETES_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.4.31/kubernetes-0.4.31.squashfs
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.4.31/5.3.18-150300.59.87-default-0.4.31.kernel
-    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.4.31/initrd.img-0.4.31.xz
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.4.32/kubernetes-0.4.32.squashfs
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.4.32/5.3.18-150300.59.87-default-0.4.32.kernel
+    https://artifactory.algol60.net/artifactory/csm-images/stable/kubernetes/0.4.32/initrd.img-0.4.32.xz
 )
 
 STORAGE_CEPH_ASSETS=(
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.4.31/storage-ceph-0.4.31.squashfs
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.4.31/5.3.18-150300.59.87-default-0.4.31.kernel
-    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.4.31/initrd.img-0.4.31.xz
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.4.32/storage-ceph-0.4.32.squashfs
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.4.32/5.3.18-150300.59.87-default-0.4.32.kernel
+    https://artifactory.algol60.net/artifactory/csm-images/stable/storage-ceph/0.4.32/initrd.img-0.4.32.xz
 )
 
 HPE_SIGNING_KEY=https://arti.hpc.amslabs.hpecorp.net/artifactory/dst-misc-stable-local/SigningKeys/HPE-SHASTA-RPM-PROD.asc
