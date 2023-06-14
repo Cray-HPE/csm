1d5bb1d960cedc65250b042ad8ddbd8e0b2fbfe3
Pete Halseth <peter.halseth@hpe.com>
2023-06-13 22:19:15 -0500

    another embedded repo exlusion update

--- release.sh
+++ release.sh
@@ -279,7 +279,6 @@ if [[ "${EMBEDDED_REPO_ENABLED:-yes}" = "yes" ]]; then
     cat "${ROOTDIR}/rpm/pit.rpm-list" "${ROOTDIR}/rpm/images.rpm-list" \
     | sort -u \
     | grep -v gpg-pubkey \
-    | grep -v google-guest-agent \
     | grep -v conntrack-1.1.x86_64 \
     | "${ROOTDIR}/hack/gen-rpm-index.sh" \
     > "${ROOTDIR}/rpm/embedded.yaml"
