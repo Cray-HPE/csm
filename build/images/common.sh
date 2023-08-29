fe285123177b8202ff2cc5a9b9394318a62d712f
Michael Tupitsyn <mikhail.tupitsyn@hpe.com>
2023-08-29 16:04:55 -0700

    Use cached skopeo image in build/images

--- build/images/common.sh
+++ build/images/common.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-: "${SKOPEO_IMAGE:=quay.io/skopeo/stable:v1.4.1}"
+: "${SKOPEO_IMAGE:=artifactory.algol60.net/csm-docker/stable/quay.io/skopeo/stable:v1.4.1}"
 : "${YQ_IMAGE:=artifactory.algol60.net/docker.io/mikefarah/yq:4}"
 
 if [ -z "${ARTIFACTORY_USER}" ] || [ -z "${ARTIFACTORY_TOKEN}" ]; then
