b16cf56b208d316c32312af690d1991a43853a77
Michael Tupitsyn <mikhail.tupitsyn@hpe.com>
2023-08-31 09:38:58 -0700

    Squashed 'vendor/github.hpe.com/hpe/hpc-shastarelm-release/' changes from 4525f48d..f49f4950
    f49f4950 DST-11708: add set -e back to the end of extract-from-container function, if it was in effect before the call (#51)
    f37f6ced Merge pull request #52 from hpe/feature/skopeo-1.13
    4aa958c4 CASMINST-6629 Use skopeo 1.13.2
    
    git-subtree-dir: vendor/github.hpe.com/hpe/hpc-shastarelm-release
    git-subtree-split: f49f49503d189894c74145095dfbe5d5a0f91e98

--- build/images/common.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-: "${SKOPEO_IMAGE:=quay.io/skopeo/stable:v1.4.1}"
-: "${YQ_IMAGE:=artifactory.algol60.net/docker.io/mikefarah/yq:4}"
-
-if [ -z "${ARTIFACTORY_USER}" ] || [ -z "${ARTIFACTORY_TOKEN}" ]; then
-    echo "Missing authentication information for image download. Please set ARTIFACTORY_USER and ARTIFACTORY_TOKEN environment variables."
-    exit 1
-fi
--- build/images/pull_images.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e -o pipefail
-
-SRCDIR="$(dirname "${BASH_SOURCE[0]}")"
-. "${SRCDIR}/common.sh"
-
-docker pull "${YQ_IMAGE}"
-docker pull "${SKOPEO_IMAGE}"
