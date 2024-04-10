92122e2fdf190ce0c4235315f295969d7aef9497
Mikhail Tupitsyn <mikhail.tupitsyn@hpe.com>
2024-04-10 13:40:08 -0700

    CASMINST-6840 Include aarch64 versions of docker images if they exist.
    (cherry picked from commit 80afe3f8db578924ba916eef0d764718ec8faad0)

--- Makefile
+++ Makefile
@@ -28,7 +28,7 @@ clean:
 # Pre-flight checks
 pre-flight-check:
 	$(call header,"Executing pre-flight checks ...")
-	@for t in curl wget skopeo yq jq parallel timeout rsync rpm rpm2cpio cpio docker; do echo -ne "Checking $$t ... "; which $$t; done
+	@for t in curl wget yq jq parallel timeout rsync rpm rpm2cpio cpio docker; do echo -ne "Checking $$t ... "; which $$t; done
 	./get_base.sh
 
 # Validate assets (node images - ISO, squashfs, etc)
@@ -77,14 +77,15 @@ $(BUILDDIR)/docker:
 	cp "build/images/index.txt" "dist/$(RELEASE)-images.txt"
 
 # Snyk scan of images directory
+# Depends on build/images/index.txt file, produced by valudate-images
 .PHONY: snyk
-snyk: images
+snyk: validate-images
 	$(call header,"Performing Snyk scan for container images in $(BUILDDIR)/docker")
 	@$(MAKE) $(BUILDDIR)/scans
 $(BUILDDIR)/scans:
 	parallel -j $(PARALLEL_JOBS) --halt-on-error now,fail=1 -v \
 		--ungroup -a build/images/index.txt --colsep '\t' \
-		hack/snyk-scan.sh '{1}' '{2}' "$(BUILDDIR)/docker" "$(BUILDDIR)/scans/docker"
+		hack/snyk-scan.sh '{1}' '{2}' "$(BUILDDIR)/scans/docker"
 	cp build/images/chartmap.csv "$(BUILDDIR)/scans/docker/"
 	hack/snyk-aggregate-results.sh "$(BUILDDIR)/scans/docker" --helm-chart-map "/data/chartmap.csv" --sheet-name "$(RELEASE)"
 	hack/snyk-to-html.sh "$(BUILDDIR)/scans/docker"
--- build/images/sync.sh
+++ build/images/sync.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 ROOTDIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../..")
 source "${ROOTDIR}/common.sh"
+source "${ROOTDIR}/vendor/github.hpe.com/hpe/hpc-shastarelm-release/lib/release.sh"
 
 function usage() {
     echo >&2 "usage: ${0##*/} LOGICAL_IMAGE PHYSICAL_IMAGE DIR"
@@ -16,43 +17,6 @@ logical_image="${1}"
 physical_image="${2}"
 destdir="${3%/}/$logical_image"
 
-function skopeo-copy() {
-    sha="${physical_image/*:}"
-    if test -f "${destdir}/manifest.json" && echo "${sha}" "${destdir}/manifest.json" | sha256sum -c -; then
-        echo >&2 "+ Valid checksum found for ${destdir}/manifest.json, skip copy"
-    else
-        echo >&2 "+ skopeo copy docker://$physical_image dir:$destdir"
-
-        # Sync to temporary working directory in case of error
-        workdir="$(mktemp -d .skopeo-copy-XXXXXXX)"
-        trap "rm -fr '$workdir'" EXIT
-
-        creds=""
-        [[ "${physical_image}" == artifactory.algol60.net/* ]] && creds="${ARTIFACTORY_USER}:${ARTIFACTORY_TOKEN}"
-
-        skopeo \
-            --command-timeout 60s \
-            copy \
-            --override-os linux \
-            --override-arch amd64 \
-            --retry-times 5 \
-            ${creds:+--src-creds "${creds}"} \
-            "docker://$physical_image" \
-            "dir:${workdir}" \
-            >&2 || exit 255
-
-        # Ensure intermediate directories exist
-        mkdir -p "$(dirname "$destdir")"
-
-        # Ensure destination directory is fresh, which is particularly important
-        # if there was a previous run
-        [[ -e "$destdir" ]] && rm -fr "$destdir"
-
-        # Move image to destination directory
-        mv "$workdir" "$destdir"
-    fi
-}
-
 if [ -n "${CSM_BASE_VERSION:-}" ]; then
     base_image_dir="${ROOTDIR}/dist/csm-${CSM_BASE_VERSION}/docker/${logical_image}/"
     if [ -d "${base_image_dir}" ]; then
@@ -60,8 +24,13 @@ if [ -n "${CSM_BASE_VERSION:-}" ]; then
         echo >&2 "+ rsync -aq ${base_image_dir} ${destdir%/}/"
         rsync -aq "${base_image_dir}" "${destdir%/}/"
     else
-        skopeo-copy
+        sha="${physical_image/*:}"
+        if test -f "${destdir}/manifest.json" && echo "${sha}" "${destdir}/manifest.json" | sha256sum -c -; then
+            echo >&2 "+ Valid checksum found for ${destdir}/manifest.json, skip copy"
+        else
+            skopeo-copy "docker://$physical_image" "dir:${destdir}"
+        fi
     fi
 else
-    skopeo-copy
+    skopeo-copy "docker://$physical_image" "dir:${destdir}"
 fi
