bdb0cf6da9b14939e0cbb6f149a369b0a09d51f6
Mikhail Tupitsyn <mikhail.tupitsyn@hpe.com>
2024-05-08 10:09:05 -0700

    Revert "Validate checksum on image from CSM_BASE_VERSION"
    This reverts commit 220f0546c2f02dd300efad297743a38cfc659c0f.

--- build/images/sync.sh
+++ build/images/sync.sh
@@ -19,16 +19,17 @@ destdir="${3%/}/$logical_image"
 
 if [ -n "${CSM_BASE_VERSION:-}" ]; then
     base_image_dir="${ROOTDIR}/dist/csm-${CSM_BASE_VERSION}/docker/${logical_image}/"
-    sha="${physical_image/*:}"
-    if test -f "${destdir}/manifest.json" && echo "${sha}" "${destdir}/manifest.json" | sha256sum -c -; then
-        echo >&2 "+ Valid checksum found in ${destdir}/manifest.json, skip copy"
-    elif test -f "${base_image_dir}" && echo "${sha}" "${base_image_dir}/manifest.json" | sha256sum -c -; then
+    if [ -d "${base_image_dir}" ]; then
         mkdir -p "${destdir}"
-        echo >&2 "+ Valid checksum found in ${base_image_dir}/manifest.json, doing rsync -aq ${base_image_dir} ${destdir%/}/"
+        echo >&2 "+ rsync -aq ${base_image_dir} ${destdir%/}/"
         rsync -aq "${base_image_dir}" "${destdir%/}/"
     else
-        echo >&2 "+ Valid checksum is not found in ${base_image_dir}/manifest.json, pulling from external source"
-        skopeo-copy "docker://$physical_image" "dir:${destdir}"
+        sha="${physical_image/*:}"
+        if test -f "${destdir}/manifest.json" && echo "${sha}" "${destdir}/manifest.json" | sha256sum -c -; then
+            echo >&2 "+ Valid checksum found for ${destdir}/manifest.json, skip copy"
+        else
+            skopeo-copy "docker://$physical_image" "dir:${destdir}"
+        fi
     fi
 else
     skopeo-copy "docker://$physical_image" "dir:${destdir}"
