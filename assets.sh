ae5d2da46b8c71889802c71dfab267a74a29c363
Mikhail Tupitsyn <mikhail.tupitsyn@hpe.com>
2025-01-15 10:39:28 -0800

    CASM-4937: Nightly Builds Phase 2: trigger builds - minor adjustments

--- assets.sh
+++ assets.sh
@@ -31,19 +31,19 @@ NCN_ARCH='x86_64'
 CN_ARCH=("x86_64")
 
 # All images must use the same, exact kernel version.
-KERNEL_VERSION='6.4.0-*-default'
+KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=6.2.*
+KUBERNETES_IMAGE_ID=6.2.35
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=6.2.*
+PIT_IMAGE_ID=6.2.35
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=6.2.*
+STORAGE_CEPH_IMAGE_ID=6.2.35
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=6.2.*
+COMPUTE_IMAGE_ID=6.2.35
 
 # Public keys for RPM signature validation.
 #
