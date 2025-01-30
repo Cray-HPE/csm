cea9ef7204fecf417ee1abcc4ce7536a68893692
studenym-hpe <maureen.studeny@hpe.com>
2025-01-30 09:12:54 -0600

    CASMPET-7631: Use etcd 3.5.16 in node images (#3890)

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=983e633-1738157476520
+KUBERNETES_IMAGE_ID=187f243-1738182776328
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=983e633-1738157476520
+PIT_IMAGE_ID=187f243-1738182776328
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=983e633-1738157476520
+STORAGE_CEPH_IMAGE_ID=187f243-1738182776328
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=983e633-1738157476520
+COMPUTE_IMAGE_ID=187f243-1738182776328
 
 # Public keys for RPM signature validation.
 #
