eb64ce487198b8cc68c71e8de32d77f06cdc5278
David Fluck <david.fluck@hpe.com>
2025-01-30 17:31:58 -0800

    * Move deprecated KUBELET_EXTRA_ARGS flags into kubeadm.cfg as part of   KubeletConfiguration, namely: --container-runtime-endpoint, and   --runtime-request-timeout.
    * Remove --container-runtime from KUBELET_EXTRA_ARGS entirely, as it is
      unavailable as of Kubernetes 1.27.0.
    
    * Remove KUBELET_EXTRA_ARGS entirely, as there are no extra arguments
      left.

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=187f243-1738182776328
+KUBERNETES_IMAGE_ID=60de6ae-1738276494695
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=187f243-1738182776328
+PIT_IMAGE_ID=60de6ae-1738276494695
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=187f243-1738182776328
+STORAGE_CEPH_IMAGE_ID=60de6ae-1738276494695
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=187f243-1738182776328
+COMPUTE_IMAGE_ID=60de6ae-1738276494695
 
 # Public keys for RPM signature validation.
 #
