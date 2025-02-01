3a4b3d3abfde5136992a354a8cb4a10e8eb2a2a0
Maureen Studeny <maureen.studeny@hpe.com>
2025-02-01 07:44:14 -0600

    CASMPET-7333: Update kubeadm config to use kubeadm.k8s.io/v1beta4 in InitConfiguration and ClusterConfiguration

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=60de6ae-1738276494695
+KUBERNETES_IMAGE_ID=ba2349f-1738371226862
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=60de6ae-1738276494695
+PIT_IMAGE_ID=ba2349f-1738371226862
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=60de6ae-1738276494695
+STORAGE_CEPH_IMAGE_ID=ba2349f-1738371226862
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=60de6ae-1738276494695
+COMPUTE_IMAGE_ID=ba2349f-1738371226862
 
 # Public keys for RPM signature validation.
 #
