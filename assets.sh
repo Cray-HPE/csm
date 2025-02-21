228c0db06dcd0fccc7bfa2bdfc999161eb6c97b3
Maureen Studeny <maureen.studeny@hpe.com>
2025-02-21 16:00:37 -0600

    Use K8s 1.32.2 image 2d9b343-1740167257976

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=e125509-1739570604342
+KUBERNETES_IMAGE_ID=2d9b343-1740167257976
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=e125509-1739570604342
+PIT_IMAGE_ID=2d9b343-1740167257976
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=e125509-1739570604342
+STORAGE_CEPH_IMAGE_ID=2d9b343-1740167257976
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=e125509-1739570604342
+COMPUTE_IMAGE_ID=2d9b343-1740167257976
 
 # Public keys for RPM signature validation.
 #
