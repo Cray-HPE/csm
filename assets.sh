747142f50df65074882aec51613217e83e589765
Maureen Studeny <maureen.studeny@hpe.com>
2025-02-24 11:13:13 -0600

    Update to use cray-kyverno 1.7.1; Revert back to k8s 1.32.0

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=2d9b343-1740167257976
+KUBERNETES_IMAGE_ID=e125509-1739570604342
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=2d9b343-1740167257976
+PIT_IMAGE_ID=e125509-1739570604342
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=2d9b343-1740167257976
+STORAGE_CEPH_IMAGE_ID=e125509-1739570604342
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=2d9b343-1740167257976
+COMPUTE_IMAGE_ID=e125509-1739570604342
 
 # Public keys for RPM signature validation.
 #
