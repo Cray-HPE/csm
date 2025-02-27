1a3b93356382eaaac1b5b891b01b12d93d802c11
studenym-hpe <maureen.studeny@hpe.com>
2025-02-27 14:35:10 -0600

    CASMPET-7371: Remove PSP enablement from node-images (#3959)
    * CASMPET-7371: Remove PSP enablement from node-images
    CASMPET-7397: Remove k8s_psp_enabled_check goss test from csm-testing
    
    * Update gos-servers version to 1.19.13

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=7.0.9
+KUBERNETES_IMAGE_ID=7.0.10
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=7.0.9
+PIT_IMAGE_ID=7.0.10
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=7.0.9
+STORAGE_CEPH_IMAGE_ID=7.0.10
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=7.0.9
+COMPUTE_IMAGE_ID=7.0.10
 
 # Public keys for RPM signature validation.
 #
