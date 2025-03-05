f383282970170d5ccb4a4bf5431dff7ec5594ddb
Jason Davis <jason.davis@hpe.com>
2025-03-05 16:47:50 -0700

    MTL-2547 USS-3710 CASMTRIAGE-7917 CASM-5285

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=7.0.12
+KUBERNETES_IMAGE_ID=7.0.13
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=7.0.12
+PIT_IMAGE_ID=7.0.13
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=7.0.12
+STORAGE_CEPH_IMAGE_ID=7.0.13
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=7.0.12
+COMPUTE_IMAGE_ID=7.0.13
 
 # Public keys for RPM signature validation.
 #
