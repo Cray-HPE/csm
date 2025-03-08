8bdc46092e6c3f536862d2314bd95a12a2f61b6b
Maureen Studeny <maureen.studeny@hpe.com>
2025-03-08 07:56:49 -0600

    Use node-images based on 7.0.12 (99fc4fb-1741387937809) and adjust rpm index.yaml accordingly

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=4421b73-1741300273869
+KUBERNETES_IMAGE_ID=99fc4fb-1741387937809
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=4421b73-1741300273869
+PIT_IMAGE_ID=99fc4fb-1741387937809
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=4421b73-1741300273869
+STORAGE_CEPH_IMAGE_ID=99fc4fb-1741387937809
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=4421b73-1741300273869
+COMPUTE_IMAGE_ID=99fc4fb-1741387937809
 
 # Public keys for RPM signature validation.
 #
