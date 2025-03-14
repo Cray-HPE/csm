110bf2eb7ef7b4d9489cc2f49ad728ff04e55d2d
Maureen Studeny <maureen.studeny@hpe.com>
2025-03-14 18:17:08 -0500

    Use image 7.1.0 in csm-images/stable

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=b02866f-1741970386231
+KUBERNETES_IMAGE_ID=7.1.0
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=b02866f-1741970386231
+PIT_IMAGE_ID=7.1.0
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=b02866f-1741970386231
+STORAGE_CEPH_IMAGE_ID=7.1.0
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=b02866f-1741970386231
+COMPUTE_IMAGE_ID=7.1.0
 
 # Public keys for RPM signature validation.
 #
