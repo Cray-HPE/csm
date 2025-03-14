a9c3d601c214814e8b176026fb0cb0e20f20304a
David Fluck <david.fluck@hpe.com>
2025-03-14 12:07:48 -0700

    * Temporarily remove molly deployment step.
    * Add K8s 1.25.16 images (and dependencies) to docker/index.yaml, as
      well as to the manifests/platform.yaml pre-cache images list.
    
    * Update assets.sh image IDs to 289b474-1742338567283.
    
    * Temporarily use unstable Artifactory repositories so the build
      succeeds.
    
    * Temporarily set DOCS_CSM_VERSION to the latest version corresponding
      to release/1.6.
    
    * Downgrade craycli 0.92.0 to craycli 0.91.0 as the 0.92.0 release is
      gone.

--- assets.sh
+++ assets.sh
@@ -33,16 +33,16 @@ CN_ARCH=("x86_64" "aarch64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=7.1.1
+KUBERNETES_IMAGE_ID=289b474-1742338567283
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=7.1.1
+PIT_IMAGE_ID=289b474-1742338567283
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=7.1.1
+STORAGE_CEPH_IMAGE_ID=289b474-1742338567283
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=7.1.1
+COMPUTE_IMAGE_ID=289b474-1742338567283
 
 # Public keys for RPM signature validation.
 #
