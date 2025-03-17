548c0805f3dbe148399e69f71d2c9e7aae8aa3e2
David Fluck <david.fluck@hpe.com>
2025-03-17 12:29:35 -0700

    * Update assets.sh image IDs to 7661f90-1741993175008.

--- assets.sh
+++ assets.sh
@@ -34,16 +34,16 @@ CN_ARCH=("x86_64")
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=a0a8ef3-1741908226284
+KUBERNETES_IMAGE_ID=7661f90-1741993175008
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=a0a8ef3-1741908226284
+PIT_IMAGE_ID=7661f90-1741993175008
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=a0a8ef3-1741908226284
+STORAGE_CEPH_IMAGE_ID=7661f90-1741993175008
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=a0a8ef3-1741908226284
+COMPUTE_IMAGE_ID=7661f90-1741993175008
 
 # Public keys for RPM signature validation.
 #
