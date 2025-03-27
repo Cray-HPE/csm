e1c08bc46b7df55128547f00f6c81f1d085a8ba2
Jason Davis <106175251+jpdavis-prof@users.noreply.github.com>
2025-03-27 14:08:35 -0600

    CASMTRIAGE-8000 (#4023)
    CASMTRIAGE-8006
    MTL-2554
    CASMPET-6937
    CASMCMS-9333

--- assets.sh
+++ assets.sh
@@ -27,23 +27,22 @@
 NCN_ARCH='x86_64'
 
 # Application image architecture (including compute)
-#CN_ARCH=("x86_64" "aarch64")
-CN_ARCH=("x86_64")
+CN_ARCH=("x86_64" "aarch64")
 
 # All images must use the same, exact kernel version.
 KERNEL_VERSION='6.4.0-150600.23.17-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=7.1.2
+KUBERNETES_IMAGE_ID=7.1.6
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=7.1.2
+PIT_IMAGE_ID=7.1.6
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=7.1.2
+STORAGE_CEPH_IMAGE_ID=7.1.6
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=7.1.2
+COMPUTE_IMAGE_ID=7.1.6
 
 # Public keys for RPM signature validation.
 #
