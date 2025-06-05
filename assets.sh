62611dc1ea9a03a223f22217a2f633b672567473
ethanholen-hpe <167254217+ethanholen-hpe@users.noreply.github.com>
2025-06-05 10:44:49 -0600

    CRAYSAT-1979: updated sat version (#4166)
    * CRAYSAT-1979: updated sat version
    
    * "CRAYSAT-1979: updated node-images version to 7.1.19"
    
    * "CRAYSAT-1979: kernel changes"
    
    ---------
    
    Co-authored-by: Michael Tupitsyn <mikhail.tupitsyn@hpe.com>

--- assets.sh
+++ assets.sh
@@ -33,16 +33,16 @@ CN_ARCH=("x86_64" "aarch64")
 KERNEL_VERSION='6.4.0-150600.23.47-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=7.1.18
+KUBERNETES_IMAGE_ID=7.1.19
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=7.1.18
+PIT_IMAGE_ID=7.1.19
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=7.1.18
+STORAGE_CEPH_IMAGE_ID=7.1.19
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=7.1.18
+COMPUTE_IMAGE_ID=7.1.19
 
 # Public keys for RPM signature validation.
 #
