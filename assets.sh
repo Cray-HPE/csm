44f693f58137b653db2b0144f92b114f72b3061a
Jason Davis <106175251+jpdavis-prof@users.noreply.github.com>
2025-08-05 15:53:41 -0600

    CASMTRIAGE-8585 (#4288)

--- assets.sh
+++ assets.sh
@@ -33,16 +33,16 @@ CN_ARCH=("x86_64" "aarch64")
 KERNEL_VERSION='6.4.0-150600.23.53-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=7.1.36
+KUBERNETES_IMAGE_ID=7.1.37
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=7.1.36
+PIT_IMAGE_ID=7.1.37
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=7.1.36
+STORAGE_CEPH_IMAGE_ID=7.1.37
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=7.1.36
+COMPUTE_IMAGE_ID=7.1.37
 
 # Public keys for RPM signature validation.
 #
