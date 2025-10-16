391fddc9b3bf8f8a41c14533743cf8e31f6881be
Maureen Studeny <maureen.studeny@hpe.com>
2025-10-16 12:03:40 -0500

    CASMNET-2120: Use asset image IDs with multus-cni v4.2.2 (#4347)
    * Update multus-cni image in index and precache to v4.2.2

--- assets.sh
+++ assets.sh
@@ -33,16 +33,16 @@ CN_ARCH=("x86_64" "aarch64")
 KERNEL_VERSION='6.4.0-150700.53.16-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=7.2.2
+KUBERNETES_IMAGE_ID=7.2.3
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=7.2.2
+PIT_IMAGE_ID=7.2.3
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=7.2.2
+STORAGE_CEPH_IMAGE_ID=7.2.3
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=7.2.2
+COMPUTE_IMAGE_ID=7.2.3
 
 # Public keys for RPM signature validation.
 #
