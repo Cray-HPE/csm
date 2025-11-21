39224406112c4ccfea76c9233d6c28447ab15115
Jason Davis <106175251+jpdavis-prof@users.noreply.github.com>
2025-11-20 17:05:53 -0700

    CASMTRIAGE-8892 / MTL-2610 (#4370)
    * CASMTRIAGE-8892
    
    * MTL-2610
    
    ---------
    
    Co-authored-by: github-actions-runner <github-actions@github.com>

--- assets.sh
+++ assets.sh
@@ -30,19 +30,19 @@ NCN_ARCH='x86_64'
 CN_ARCH=("x86_64" "aarch64")
 
 # All images must use the same, exact kernel version.
-KERNEL_VERSION='6.4.0-150700.53.19-default'
+KERNEL_VERSION='6.4.0-150700.53.22-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=7.2.7
+KUBERNETES_IMAGE_ID=7.2.9
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=7.2.7
+PIT_IMAGE_ID=7.2.9
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=7.2.7
+STORAGE_CEPH_IMAGE_ID=7.2.9
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=7.2.7
+COMPUTE_IMAGE_ID=7.2.9
 
 # Public keys for RPM signature validation.
 #
