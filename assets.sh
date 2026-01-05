7b296ed10afc3ff9a04adf7994cd572e816a982e
Chris Spiller <86013738+spillerc-hpe@users.noreply.github.com>
2026-01-05 20:33:13 +0000

    CASMNET-2380 - CANU: NMN Isolation ACLs on Leafs break bgp and ssh (#4404)

--- assets.sh
+++ assets.sh
@@ -33,16 +33,16 @@ CN_ARCH=("x86_64" "aarch64")
 KERNEL_VERSION='6.4.0-150700.53.22-default'
 
 # The image ID may not always match the other images and should be defined individually.
-KUBERNETES_IMAGE_ID=7.2.14
+KUBERNETES_IMAGE_ID=7.2.15
 
 # The image ID may not always match the other images and should be defined individually.
-PIT_IMAGE_ID=7.2.14
+PIT_IMAGE_ID=7.2.15
 
 # The image ID may not always match the other images and should be defined individually.
-STORAGE_CEPH_IMAGE_ID=7.2.14
+STORAGE_CEPH_IMAGE_ID=7.2.15
 
 # The image ID may not always match the other images and should be defined individually.
-COMPUTE_IMAGE_ID=7.2.14
+COMPUTE_IMAGE_ID=7.2.15
 
 # Public keys for RPM signature validation.
 #
