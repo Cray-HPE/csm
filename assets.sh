3b2b347038ed9e93afd2183509eab687391c1b0e
Michael Tupitsyn <mikhail.tupitsyn@hpe.com>
2026-05-21 07:40:18 -0700

    Update assets.sh - fix typo (truee vs true)

--- assets.sh
+++ assets.sh
@@ -25,7 +25,7 @@
 
 # Set this to "false" to skip NCN images aka assets (for example, in a service pack).
 # Skipping assets will also skip "embedded RPMs" (set of RPMs matching to list of RPMs pre-installed on NCN images).
-CSM_USE_ASSETS='truee'
+CSM_USE_ASSETS='true'
 
 # Multi-arch management clusters are not supported.
 NCN_ARCH='x86_64'
